ea41c4f61565567fbb5daaf56f6e1349f59eb907
Kenichi Takahashi <kenichi.taka@gmail.com>
2017-01-23 20:14:28 +0900

    craete bin directory

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@
 MITAMAE_BIN="mitamae-x86_64-darwin"
 MITAMAE_VERSION="1.3.2"
 
-if [ ! -e ./${MITAMAE_BIN} ]; then
+if [ ! -e bin/${MITAMAE_BIN} ]; then
   curl -LJO https://github.com/k0kubun/mitamae/releases/download/v${MITAMAE_VERSION}/${MITAMAE_BIN}.tar.gz
-  tar -zxf ${MITAMAE_BIN}.tar.gz
+  tar -zxf ${MITAMAE_BIN}.tar.gz -C bin
+  rm ${MITAMAE_BIN}.tar.gz
 fi
 
-./${MITAMAE_BIN} local --node-yaml node.yaml recipe.rb
+bin/${MITAMAE_BIN} local --node-yaml node.yaml recipe.rb
